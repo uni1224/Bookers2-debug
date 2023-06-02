b3891cadc942d35d8198862d338a8395f57f7e93
EC2 Default User <ec2-user@ip-172-31-20-175.ap-southeast-2.compute.internal>
2023-06-02 08:25:09 +0000

    Edited

--- bookers2-debug/app/models/user.rb
+++ bookers2-debug/app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  
+   has_many :followings, through: :relationships, source: :followed
+   has_many :followers, through: :reverse_of_relationships, source: :follower
+
+
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length:{ maximum: 50}
@@ -20,4 +28,15 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+  def follow(user_id)
+  relationships.create(followed_id: user_id)
+  end
+# フォローを外すときの処理
+def unfollow(user_id)
+  relationships.find_by(followed_id: user_id).destroy
+end
+# フォローしているか判定
+def following?(user)
+  followings.include?(user)
+end
 end
--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  devise_for :users
   root to: "home#top"
@@ -10,5 +12,9 @@ Rails.application.routes.draw do
    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
  end
