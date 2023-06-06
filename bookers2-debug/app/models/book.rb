b59409d2e6f5efea873c06797d4da1fc9db58c7a
uni1224 <saekahane99@gmail.com>
2023-06-06 06:27:39 +0000

    Edited

--- bookers2-debug/app/models/book.rb
+++ bookers2-debug/app/models/book.rb
@@ -3,6 +3,7 @@ belongs_to :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
