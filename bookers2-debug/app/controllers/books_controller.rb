52e10913a2b6e89fadfa3b12fbd55890a8f509a8
uni1224 <saekahane99@gmail.com>
2023-06-06 07:01:15 +0000

    Edited

--- bookers2-debug/app/controllers/books_controller.rb
+++ bookers2-debug/app/controllers/books_controller.rb
@@ -8,13 +8,17 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort_by {|x|
+        x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }.reverse
     @book = Book.new
   end
-
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
+   @book = Book.new(book_params)
+   @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
