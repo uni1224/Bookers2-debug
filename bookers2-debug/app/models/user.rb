e399842db8af02b4c861ec05d5e60091f808e13d
uni1224 <saekahane99@gmail.com>
2023-06-07 03:58:55 +0000

    Edited

--- bookers2-debug/app/models/user.rb
+++ bookers2-debug/app/models/user.rb
@@ -9,18 +9,17 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
-  
-   has_many :followings, through: :relationships, source: :followed
-   has_many :followers, through: :reverse_of_relationships, source: :follower
-
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :messages,dependent: :destroy
+  has_many :entries,dependent: :destroy
 
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length:{ maximum: 50}
 
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
